af14f824cfcffddd6777e436d13f405c4004ed57
Denison Junior <denison.junior@cebraspe.org.br>
2026-07-15 11:51:41 -0300

    feat: implement basic Rocket API with home and resource endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+use rocket::{Build, Rocket};
+use rocket::serde::{json::Json, Serialize };
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Resource {
+    id: u32,
+    title: String,
+    description: String,
 }
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Home {
+    message: String,
+    endpoints: Vec<String>,
+}
+
+#[get("/")]
+fn home() -> Json<Home> {
+    Json(
+        Home {
+            message: "Welcome to the home page".to_string(),
+            endpoints: vec![
+                "/resource".to_string(),
+            ]
+        }
+    )
+}
+
+#[get("/resource")]
+fn resource_index() -> Json<Vec<Resource>> {
+    let resourses = vec![
+        Resource {
+            id: 1,
+            title: "Sample Resource 1".to_string(),
+            description: "This is the first sample resource.".to_string()
+        },
+        Resource {
+            id: 2,
+            title: "Sample Resource 2".to_string(),
+            description: "This is the second sample resource.".to_string()
+        },
+        Resource {
+            id: 3,
+            title: "Sample Resource 3".to_string(),
+            description: "This is the third sample resource.".to_string()
+        },
+    ];
+
+    Json(resourses)
+}
+
+#[launch]
+async fn rocket() -> _ {
+    rocket::build().mount("/", routes![home, resource_index])
+}
+
+
